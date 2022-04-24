ab4a6a1d8a9ace96bdf14ebdeb40ba7d25eb744e
tobimesa03@gmail.com <tobimesa03@gmail.com>
2022-04-24 19:47:22 -0300

    Adding menu.sh

---
+++ menu.sh
@@ -0,0 +1,28 @@
+PS3='Please select source service: '
+options=("Option 1" "Option 2" "Option 3" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Cloud Builder")
+            echo "you chose choice 1"
+            ;;
+        "SDDC Manager")
+            echo "you chose choice 2"
+            ;;
+        "Administrative / Managment Networks")
+            echo "you chose choice $REPLY which is $opt"
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
+
+
+subMenuCloudBuilder () {
+    local PS3 = 'Please select the destinations service':
+    local options = ("")
+
+}
