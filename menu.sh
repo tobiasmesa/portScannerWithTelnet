269f94783c10308776acc33ac84ce6c26c5c4b85
tobiasmesa <tobiasmesa030303@gmail.com>
2022-04-24 20:57:59 -0300

    Editing menu.sh

--- menu.sh
+++ menu.sh
@@ -22,6 +22,46 @@ done
 
 
 subMenuCloudBuilder () {
-    #local PS3 = 'Please select the destinations service':
-   # local options = ("")
+    local PS3 = 'Please select the destinations service':
+    local options = ("DNS Server(s)" "ESXi Hosts" "ESXi Hosts for the Management Domain" "NSX Controllers (NSX-v) for the Management Domain" "NSX Edge Service Gateways (NSX-v) for the Management Domain" "NSX Edges Nodes for the Management Domain" "NSX Manager (NSX-v) for the Management Domain" "NSX Managers for the Management Domain" "NTP Server(s)" "Platform Service Controllers for the Management/Workload Domains" "SDDC Manager" "vCenter Server for the Management Domain" "Back")
+    local opt
+    select opt in "${options[@]}"
+    do
+        case $opt in
+            "DNS Server(s)")
+                echo "you chose sub item 1"
+                ;;
+            "SDDC Manager")
+                echo "you chose sub item 1"
+                ;;
+            "ESXi Hosts")
+                echo "you chose sub item 2"
+                ;;
+            "ESXi Hosts for the Management Domain")
+                echo "you chose sub item 2"
+                ;;
+            "NSX Controllers (NSX-v) for the Management Domain")
+                echo "you chose sub item 2"
+                ;;
+            "NSX Edge Service Gateways (NSX-v) for the Management Domain")
+                echo "you chose sub item 2"
+                ;;     
+            "NSX Managers for the Management Domain")
+                echo "you chose sub item 2"
+                ;;
+            "NTP Server(s)")
+                echo "you chose sub item 2"
+                ;;
+            "Platform Service Controllers for the Management/Workload Domains")
+                echo "you chose sub item 2"
+                ;;
+            "vCenter Server for the Management Domain")
+                echo "you chose sub item 2"
+                ;;            
+            "Go back")
+                return
+                ;;
+            *) echo "invalid option $REPLY";;
+        esac
+    done
 }
