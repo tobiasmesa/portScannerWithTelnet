a219d4d9a16d602a28330f7ed4ff3f16f4ac252f
tobias <tobiasmesa03@gmail.com>
2022-04-27 11:14:11 -0300

    Modifying menu.sh

--- scanner.sh
+++ scanner.sh
@@ -1,8 +1,10 @@
 
 #!/usr/bin/env bash
 SERVER=google.com
-PORT=80
-</dev/udp/$SERVER/$PORT
+PORT=(80, 60, 70)
+
+for $i in "${PORTS[@]}" do
+</dev/tcp/$SERVER/$PORT # TCP O UDP
 if [ "$?" -ne 0 ]; then
   echo "Connection to $SERVER on port $PORT failed"
   exit 1
@@ -10,3 +12,4 @@ else
   echo "Connection to $SERVER on port $PORT succeeded"
   exit 0
 fi
+done
