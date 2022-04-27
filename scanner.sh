532a8aba2a01529920c79b04502ab9201968b5f3
tobias <tobiasmesa03@gmail.com>
2022-04-27 10:56:05 -0300

    Modifying menu.sh

--- scanner.sh
+++ scanner.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
+#!/bin/bash timeout 2 bash -c "</dev/tcp/127.0.0.1/30"; echo $?
 
 
-timeout 2 bash -c "</dev/tcp/127.0.0.1/30"; echo $?
+#!/usr/bin/env bash
+SERVER=google.com
+PORT=80
+</dev/tcp/$SERVER/$PORT
+if [ "$?" -ne 0 ]; then
+  echo "Connection to $SERVER on port $PORT failed"
+  exit 1
+else
+  echo "Connection to $SERVER on port $PORT succeeded"
+  exit 0
+fi
