f207b6f0754c3da069a701498bf591cc045b5508
tobias <tobiasmesa03@gmail.com>
2022-04-27 11:17:16 -0300

    Modifying menu.sh

--- scanner.sh
+++ scanner.sh
@@ -8,9 +8,9 @@ do
 </dev/tcp/$SERVER/$PORT 
 if [ "$?" -ne 0 ]; then
   echo "Connection to $SERVER on port $PORT failed"
-  exit 1
+  
 else
   echo "Connection to $SERVER on port $PORT succeeded"
-  exit 0
+  
 fi
 done
