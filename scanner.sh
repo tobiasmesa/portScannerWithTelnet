04b644f7409e7af319d8ed2d6c5c344b470d1ff1
tobias <tobiasmesa03@gmail.com>
2022-04-27 11:15:57 -0300

    Modifying menu.sh

--- scanner.sh
+++ scanner.sh
@@ -3,7 +3,8 @@
 SERVER=google.com
 PORT=(80, 60, 70)
 
-for $i in "${PORT[@]}" do
+for $i in "${PORT[@]}" 
+do
 </dev/tcp/$SERVER/$PORT 
 if [ "$?" -ne 0 ]; then
   echo "Connection to $SERVER on port $PORT failed"
